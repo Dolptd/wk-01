e8ccbf686ad32f7879b5b219b8fc2c242333b50d
Dolptd <patthadol43@gmail.com>
2022-05-04 21:13:23 +0700

    W2-06 Single Page Applications Part 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import './App.css';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
-import { DISHES } from './shared/dishes';
 import { Component } from "react";
 import Main from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,13 +1,11 @@
-import React, { Component } from 'react';
-// import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './MenuComponent';
-// import DishDetail from './DishdetailComponent';
-import { DISHES } from '../shared/dishes';
-// import Header from './HeaderComponent';
-// import Footer from './FooterComponent';
 import Home from './HomeComponent';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import Menu from './MenuComponent';
 import Contact from './ContactComponent';
+import DishDetail from './DishdetailComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { Component } from 'react';
+
+import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
@@ -15,10 +13,8 @@ import { LEADERS } from '../shared/leaders';
 
 
 class Main extends Component {
-
     constructor(props) {
         super(props);
-
         this.state = {
             dishes: DISHES,
             comments: COMMENTS,
@@ -41,31 +37,25 @@ class Main extends Component {
                 />
             );
         }
+
+        const DishWithId = ({ match }) => {
+            return (
+                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+                    comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))} />
+            );
+        };
+
+
         return (
             <Switch>
                 <Route path='/home' component={HomePage} />
                 <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
                 <Route exact path='/contactus' component={Contact} />
+                <Route path='/menu/:dishId' component={DishWithId} />
                 <Redirect to="/home" />
             </Switch>
         )
-
-        // return (
-        //     <div>
-        //         <Navbar dark color="primary">
-        //             <div className="container">
-        //                 <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        //             </div>
-        //         </Navbar>
-        //         <Header />
-        //         <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-        //         <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
-        //         <Footer />
-        //     </div>
-        // );
     }
 }
-
-
 
 export default Main;
