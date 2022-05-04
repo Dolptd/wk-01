5446c354ce447843adbceda83aa6d4d968dab304
Dolptd <patthadol43@gmail.com>
2022-05-04 15:55:09 +0700

    W2-02 Functional Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from "reactstrap";
+import React from 'react';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle
+} from 'reactstrap';
+
 
 class DishDetail extends Component {
     constructor(props) {
